a3c653b6f07d0d56c77d4d247933398d75b6cf9c
mezter1337 <andreas.javert@gmail.com>
2024-10-25 12:47:39 +0200

    hopefully fixed heroku deployment error

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ft@s-9s7@b8f&4ky(x3uu#1+n!k=9ecjjzfp^z$nk#hqylxvoy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", ".herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
